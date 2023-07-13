2d04acec7cd8fca82b76cc65b513cfa267522b93
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-13 05:22:55 +0200

    Added temporary test of async attacking/workload splitting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod docker;
 mod exploit;
 
+use crate::exploit::Exploit;
+
 use colored::Colorize;
+use std::sync::Arc;
+use tokio::task::JoinSet;
 
 pub enum DockerErrors {
     Build(String),
@@ -50,24 +54,52 @@ fn handle_docker_errors(e: DockerErrors) {
     }
 }
 
+// Simulate attacking 100 teams using all (10) containers
+async fn tick_attack_test(exp: &mut Arc<exploit::Exploit>, flag_store: String) {
+    let ips = (1..=100)
+        .map(|n| format!("10.0.{n}.2"))
+        .collect::<Vec<String>>();
+
+    let containers = exp.running_containers();
+
+    let mut set = JoinSet::new();
+
+    (0..containers.len()).for_each(|t: usize| {
+        let start = t * ips.len() / containers.len();
+        let end = (t + 1) * ips.len() / containers.len();
+
+        let task_ips = ips[start..end].to_vec();
+
+        set.spawn({
+            let exp = Arc::clone(&exp);
+            let flag_store = flag_store.clone();
+
+            async move {
+                for ip in task_ips {
+                    let _output = match exp.containers[t].run(ip.to_string(), &flag_store).await {
+                        Ok(output) => output,
+                        Err(e) => return handle_docker_errors(e),
+                    };
+
+                    // println!("{output}");
+                }
+            }
+        });
+    });
+
+    while let Some(_) = set.join_next().await {}
+}
+
 #[tokio::main]
 async fn main() {
-    let exp = match exploit::Exploit::init("test", 1, true).await {
+    let exp = match Exploit::init("test", 10, true).await {
         Ok(exp) => exp,
         Err(e) => return handle_docker_errors(e),
     };
 
-    let output = match exp
-        .run(
-            &exp.containers[0], // TODO: Smarter way to do this, maybe som automated selector inside the run function
-            "127.0.0.1".to_string(),
-            "flagid_rfre".to_string(),
-        )
-        .await
-    {
-        Ok(output) => output,
-        Err(e) => return handle_docker_errors(e),
-    };
-
-    println!("{output}")
+    let exp = &mut Arc::new(exp);
+    // Simulate 10 attacks (10 ticks)
+    for _ in 0..10 {
+        tick_attack_test(exp, String::from("flag_store_test")).await;
+    }
 }
