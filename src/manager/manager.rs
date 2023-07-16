5f95110f4ee4dee0ebf1356a6fd7e4fc8380015d
Jonas <jonas@jole.xyz>
2023-07-16 17:36:37 +0200

    add basic manager

--- src/manager/manager.rs
+++ src/manager/manager.rs
@@ -1,3 +1,56 @@
-fn main() {
+use color_eyre::eyre::Report;
+use tokio::io::AsyncReadExt;
+use warp::Filter;
+
+mod submitter;
+
+struct Manager {}
+
+#[tokio::main]
+async fn main() -> Result<(), Report> {
+    color_eyre::install()?;
     println!("manager");
+
+    tokio::spawn(async move {
+        tcp_listener().await.unwrap();
+    })
+    .await?;
+
+    let routes = warp::any().map(|| "Hello, World!");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+
+    Ok(())
+}
+
+async fn tcp_listener() -> Result<(), Report> {
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8001").await?;
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            // read everything for 30s, then timeout if it is not closed
+            let read_text =
+                tokio::time::timeout(std::time::Duration::from_secs(30), read_all(socket)).await;
+
+            dbg!(String::from_utf8(read_text.unwrap().unwrap()));
+        });
+    }
+}
+
+async fn read_all(mut socket: tokio::net::TcpStream) -> Result<Vec<u8>, std::io::Error> {
+    let mut buf = [0; 1024];
+    let mut read_text = Vec::new();
+    loop {
+        match socket.read(&mut buf).await {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                read_text.extend(&buf[..n]);
+            }
+            Err(e) => {
+                return Result::Err(e);
+            }
+        }
+    }
+    Ok(read_text)
 }
---
+++ src/manager/submitter.rs
@@ -0,0 +1,32 @@
+use async_trait::async_trait;
+use thiserror::Error;
+
+// implementations
+mod ecsc;
+
+/// Did not manage to submit
+#[derive(Error, Debug)]
+pub enum SubmitError {
+    #[error("Network error")]
+    NetworkError(#[from] std::io::Error),
+    #[error("Format error")]
+    /// The format of the response was not as expected
+    FormatError,
+}
+
+/// Adapted from https://web.archive.org/web/20230325144340/https://docs.ecsc2022.eu/ad_platform/
+pub enum FlagStatus {
+    Accepted,
+    Duplicate,
+    Own,
+    Old,
+    Invalid,
+    /// Server refused flag
+    Error,
+}
+
+/// Implements the low-level operation of submitting a bunch of flags
+#[async_trait]
+pub trait Submitter {
+    async fn submit(&self, flags: Vec<String>) -> Result<Vec<(String, FlagStatus)>, SubmitError>;
+}
