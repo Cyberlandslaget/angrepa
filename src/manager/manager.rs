c13dc10e1762d68faec3a6b0a6c40fa1e9235b68
Jonas <jonas@jole.xyz>
2023-07-19 19:55:20 +0200

    moooove code

--- src/manager/manager.rs
+++ src/manager/manager.rs
@@ -1,97 +1,14 @@
 use color_eyre::Report;
 use futures::future::join_all;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
-use tokio::select;
-use tokio::task::spawn;
 
 mod submitter;
-use submitter::FlagStatus;
+use submitter::Submitters;
 
 mod listener;
 use listener::{Tcp, Web};
 
-use crate::submitter::{Submitter, Submitters};
-
-struct SubmitterManager<S> {
-    submitter: S,
-    flag_regex: Regex,
-}
-
-impl<S> SubmitterManager<S>
-where
-    S: Submitter + Send + Sync + Clone + 'static,
-{
-    /// Submits flags
-    async fn submit(
-        submitter: S,
-        flags: Vec<String>,
-        result_tx: flume::Sender<(String, FlagStatus)>,
-    ) {
-        println!("Submitting {:?}", flags);
-        let results = submitter.submit(flags).await.unwrap();
-        for res in results {
-            result_tx.send_async(res).await.unwrap();
-        }
-    }
-
-    /// Extracts out flags
-    async fn getter(
-        raw_flag_rx: flume::Receiver<String>,
-        parsed_tx: flume::Sender<String>,
-        flag_regex: Regex,
-    ) {
-        while let Ok(raw) = raw_flag_rx.recv_async().await {
-            for flag in flag_regex.captures_iter(&raw) {
-                let flag = flag[0].to_string();
-                println!("Recieved flag {}", flag);
-                parsed_tx.send_async(flag).await.unwrap();
-            }
-        }
-    }
-
-    async fn run(&self, raw_flag_rx: flume::Receiver<String>) {
-        let (parsed_tx, parsed_rx) = flume::unbounded::<String>();
-        let (result_tx, result_rx) = flume::unbounded::<(String, FlagStatus)>();
-
-        spawn(SubmitterManager::<S>::getter(
-            raw_flag_rx,
-            parsed_tx,
-            self.flag_regex.clone(),
-        ));
-
-        let mut seen: HashSet<String> = HashSet::new();
-        let mut status: HashMap<String, FlagStatus> = HashMap::new();
-        let mut flag_queue = Vec::new();
-
-        let mut send_signal = tokio::time::interval(std::time::Duration::from_secs(5));
-        send_signal.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
-
-        loop {
-            select!(
-                _ = send_signal.tick() => {
-                    let to_submit = flag_queue.clone();
-                    flag_queue.clear();
-
-                    let result_tx = result_tx.clone();
-
-                    spawn(SubmitterManager::<S>::submit(self.submitter.clone(), to_submit, result_tx));
-                },
-                f = parsed_rx.recv_async() => {
-                    let f = f.unwrap();
-                    if seen.insert(f.clone()) {
-                        flag_queue.push(f);
-                    }
-                },
-                res = result_rx.recv_async() => {
-                    let (flag, flag_status) = res.unwrap();
-                    status.insert(flag.clone(), flag_status);
-                    println!("Got status  {}: {:?}", flag, flag_status);
-                }
-            )
-        }
-    }
-}
+mod handler;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
@@ -109,11 +26,11 @@ async fn main() -> Result<(), Report> {
     let sub = Submitters::from_conf(&config.manager)?;
 
     // set up channels
-    let (flag_tx, flag_rx) = flume::unbounded::<String>();
+    let (raw_flag_tx, raw_flag_rx) = flume::unbounded::<String>();
 
     // run tcp listener on another thread
     let tcp_handle = {
-        let flag_tx = flag_tx.clone();
+        let flag_tx = raw_flag_tx.clone();
 
         let tcp = Tcp::new(config.manager.tcp_listener.parse()?);
         tokio::spawn(async move {
@@ -123,7 +40,7 @@ async fn main() -> Result<(), Report> {
 
     // run web listener on another thread
     let web_handle = {
-        let flag_tx = flag_tx.clone();
+        let flag_tx = raw_flag_tx.clone();
         let web = Web::new(config.manager.http_listener.parse()?);
 
         tokio::spawn(async move {
@@ -135,20 +52,10 @@ async fn main() -> Result<(), Report> {
     let sub_handle = tokio::spawn(async move {
         match sub {
             Submitters::Dummy(submitter) => {
-                SubmitterManager {
-                    submitter,
-                    flag_regex,
-                }
-                .run(flag_rx)
-                .await
+                handler::run(raw_flag_rx, submitter, flag_regex).await;
             }
             Submitters::Faust(submitter) => {
-                SubmitterManager {
-                    submitter,
-                    flag_regex,
-                }
-                .run(flag_rx)
-                .await
+                handler::run(raw_flag_rx, submitter, flag_regex).await;
             }
         }
     });
