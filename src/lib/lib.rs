ff2a16cc04e6b2f01134e4b16df7415b3b37798b
Jonas <jonas@jole.xyz>
2023-07-21 15:43:04 +0200

    connection

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -2,3 +2,16 @@ pub mod config;
 pub mod models;
 pub mod schema;
 pub mod wh;
+
+use color_eyre::Report;
+use diesel::{Connection, PgConnection};
+use dotenvy::dotenv;
+use std::env;
+
+pub fn db_connect() -> Result<PgConnection, Report> {
+    dotenv()?;
+
+    let url = env::var("DATABASE_URL")?;
+
+    Ok(PgConnection::establish(&url)?)
+}
