baf482f48ed02aa5c4924b47d38bd6af231a2eb6
Jonas <jonas@jole.xyz>
2023-07-22 16:36:05 +0200

    idk

--- src/lib/schema.rs
+++ src/lib/schema.rs
@@ -35,4 +35,8 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(exploits, flags, runlogs,);
+diesel::allow_tables_to_appear_in_same_query!(
+    exploits,
+    flags,
+    runlogs,
+);
