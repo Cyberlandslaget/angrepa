8772216d2ed835f252655a39a9c0af3ead8c78b5
Jonas <jonas@jole.xyz>
2023-07-23 00:42:02 +0200

    new tables

--- src/lib/schema.rs
+++ src/lib/schema.rs
@@ -1,38 +1,38 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    exploits (id) {
-        id -> Text,
-        running -> Bool,
-        attack_target -> Nullable<Text>,
-        blacklist -> Array<Nullable<Text>>,
-        docker_image -> Text,
-        exploit_kind -> Text,
-    }
-}
-
-diesel::table! {
-    flags (flag) {
-        flag -> Text,
-        tick -> Nullable<Int4>,
-        stamp -> Nullable<Timestamp>,
-        exploit_id -> Nullable<Text>,
-        target_ip -> Nullable<Text>,
-        flagstore -> Nullable<Text>,
-        sent -> Bool,
-        status -> Nullable<Text>,
-    }
-}
-
-diesel::table! {
-    runlogs (id) {
+    execution (id) {
         id -> Int4,
-        from_exploit_id -> Text,
-        from_ip -> Text,
-        tick -> Int4,
-        stamp -> Timestamp,
-        content -> Text,
+        exploit_id -> Int4,
+        output -> Text,
+        started_at -> Timestamp,
+        finished_at -> Timestamp,
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(exploits, flags, runlogs,);
+diesel::table! {
+    exploit (id) {
+        id -> Int4,
+        name -> Text,
+        service -> Text,
+        blacklist -> Text,
+        docker_image -> Text,
+    }
+}
+
+diesel::table! {
+    flag (id) {
+        id -> Int4,
+        text -> Text,
+        status -> Text,
+        timestamp -> Timestamp,
+        execution_id -> Int4,
+        exploit_id -> Int4,
+    }
+}
+
+diesel::joinable!(execution -> exploit (exploit_id));
+diesel::joinable!(flag -> execution (execution_id));
+diesel::joinable!(flag -> exploit (exploit_id));
+
+diesel::allow_tables_to_appear_in_same_query!(execution, exploit, flag,);
