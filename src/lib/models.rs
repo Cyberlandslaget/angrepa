5be05ce311bf407776be813f073c3759853164c4
Jonas <jonas@jole.xyz>
2023-07-23 00:45:50 +0200

    update models

--- src/lib/models.rs
+++ src/lib/models.rs
@@ -1,54 +1,35 @@
 use diesel::prelude::*;
 
 #[derive(Debug, Clone, Queryable, Selectable, Insertable)]
-#[diesel(table_name = super::schema::exploits)]
+#[diesel(table_name = super::schema::exploit)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct ExploitModel {
-    pub id: String,
-    pub running: bool,
-    pub attack_target: Option<String>,
-    pub blacklist: Vec<Option<String>>,
+    pub id: i32,
+    pub name: String,
+    pub service: String,
+    pub blacklist: String,
     pub docker_image: String,
-    pub exploit_kind: String,
 }
 
 #[derive(Debug, Clone, Queryable, Selectable, Insertable)]
-#[diesel(table_name = super::schema::flags)]
+#[diesel(table_name = super::schema::execution)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct ExecutionModel {
+    pub id: i32,
+    pub exploit_id: i32,
+    pub output: String,
+    pub started_at: chrono::NaiveDateTime,
+    pub finished_at: chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Clone, Queryable, Selectable, Insertable)]
+#[diesel(table_name = super::schema::flag)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct FlagModel {
-    pub flag: String,
-    pub tick: Option<i32>,
-    pub stamp: Option<chrono::NaiveDateTime>,
-    pub exploit_id: Option<String>,
-    pub target_ip: Option<String>,
-    pub flagstore: Option<String>,
-    pub sent: bool,
-    pub status: Option<String>,
-}
-
-impl Default for FlagModel {
-    fn default() -> Self {
-        Self {
-            flag: "".to_string(),
-            tick: None,
-            stamp: None,
-            exploit_id: None,
-            target_ip: None,
-            flagstore: None,
-            sent: false,
-            status: None,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Queryable, Selectable, Insertable)]
-#[diesel(table_name = super::schema::runlogs)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct RunlogModel {
     pub id: i32,
-    pub from_exploit_id: String,
-    pub from_ip: String,
-    pub tick: i32,
-    pub stamp: chrono::NaiveDateTime,
-    pub content: String,
+    pub text: String,
+    pub status: String,
+    pub timestamp: chrono::NaiveDateTime,
+    pub execution_id: i32,
+    pub exploit_id: i32,
 }
