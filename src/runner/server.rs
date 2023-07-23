285ffdea645418c60dea21f059ed77627bdd7a16
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-23 03:27:56 +0200

    exploit upload start

--- src/runner/server.rs
+++ src/runner/server.rs
@@ -1,12 +1,16 @@
+use rocket::{self, routes};
+
+mod exploit;
 mod templates;
 
-use rocket::{self, routes};
+use exploit::*;
 use templates::*;
 
 pub async fn run() {
     // TODO: Shutdown on SIGINT
     let result = rocket::build()
         .mount("/templates", routes![list, download])
+        .mount("/exploit", routes![upload])
         .launch()
         .await;
 
