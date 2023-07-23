4ca42972e56238e681d29a4ed26c37357a87edec
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-23 20:16:14 +0200

    rocket -> axum

--- src/runner/server/templates.rs
+++ src/runner/server/templates.rs
@@ -1,28 +1,35 @@
-use rocket::http::{ContentType, Status};
-use rocket::serde::json::{json, Value};
-use rocket::{self, get};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde_json::{json, Value};
 
-#[get("/")]
-pub fn list() -> Value {
-    json!(std::fs::read_dir("./data/templates")
-        .unwrap()
-        .filter_map(|e| e.ok())
-        .map(|e| {
-            e.path()
-                .file_name()
-                .unwrap()
-                .to_os_string()
-                .into_string()
-                .unwrap()
-        })
-        .collect::<Vec<_>>())
+// #[get("/")]
+// pub fn list() -> Value {
+//     json!(std::fs::read_dir("./data/templates")
+//         .unwrap()
+//         .filter_map(|e| e.ok())
+//         .map(|e| {
+//             e.path()
+//                 .file_name()
+//                 .unwrap()
+//                 .to_os_string()
+//                 .into_string()
+//                 .unwrap()
+//         })
+//         .collect::<Vec<_>>())
+// }
+
+// #[get("/<template>")]
+// pub fn download(template: &str) -> (Status, (ContentType, Vec<u8>)) {
+//     let mut tar = tar::Builder::new(Vec::new());
+//     tar.append_dir_all(template, format!("./data/templates/{template}"))
+//         .unwrap();
+
+//     (Status::Ok, (ContentType::TAR, tar.into_inner().unwrap()))
+// }
+
+async fn root() -> (StatusCode, Json<Value>) {
+    (StatusCode::OK, json!({ "status": "ok" }).into())
 }
 
-#[get("/<template>")]
-pub fn download(template: &str) -> (Status, (ContentType, Vec<u8>)) {
-    let mut tar = tar::Builder::new(Vec::new());
-    tar.append_dir_all(template, format!("./data/templates/{template}"))
-        .unwrap();
-
-    (Status::Ok, (ContentType::TAR, tar.into_inner().unwrap()))
+pub fn router() -> Router {
+    Router::new().route("/", get(root))
 }
