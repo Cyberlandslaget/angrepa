834efaf95b62d2db00763caeed769778676c42bf
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-23 02:35:09 +0200

    templates

--- src/runner/server.rs
+++ src/runner/server.rs
@@ -2,13 +2,39 @@ use rocket::serde::json::{json, Value};
 use rocket::{self, get, routes};
 
 #[get("/templates")]
-fn test() -> Value {
-    json!({ "status": "ok" })
+fn list_templates() -> Value {
+    json!(std::fs::read_dir("./data/templates")
+        .unwrap()
+        .filter_map(|e| e.ok())
+        .map(|e| {
+            e.path()
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+        })
+        .collect::<Vec<_>>())
+}
+
+#[get("/templates/<template>")]
+fn get_template(template: &str) -> Vec<u8> {
+    use tar::Builder;
+
+    let mut tar = Builder::new(Vec::new());
+
+    tar.append_dir_all(template, format!("./data/templates/{template}"))
+        .unwrap();
+
+    tar.into_inner().unwrap()
 }
 
 pub async fn run() {
     // TODO: Shutdown on SIGINT
-    let result = rocket::build().mount("/", routes![test]).launch().await;
+    let result = rocket::build()
+        .mount("/", routes![list_templates, get_template])
+        .launch()
+        .await;
 
     result.expect("server failed unexpectedly");
 }
