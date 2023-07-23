39659ceee4d86e03e3407f3511c88cf6d26f301f
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-23 21:04:19 +0200

    working /templates/*

--- src/runner/server/templates.rs
+++ src/runner/server/templates.rs
@@ -1,35 +1,49 @@
-use axum::{http::StatusCode, routing::get, Json, Router};
+use axum::{
+    body::{Bytes, Full},
+    extract::Path,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+    Json, Router,
+};
 use serde_json::{json, Value};
 
-// #[get("/")]
-// pub fn list() -> Value {
-//     json!(std::fs::read_dir("./data/templates")
-//         .unwrap()
-//         .filter_map(|e| e.ok())
-//         .map(|e| {
-//             e.path()
-//                 .file_name()
-//                 .unwrap()
-//                 .to_os_string()
-//                 .into_string()
-//                 .unwrap()
-//         })
-//         .collect::<Vec<_>>())
-// }
+async fn list() -> Json<Value> {
+    json!(std::fs::read_dir("./data/templates")
+        .unwrap()
+        .filter_map(|e| e.ok())
+        .map(|e| {
+            e.path()
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+        })
+        .collect::<Vec<_>>())
+    .into()
+}
 
-// #[get("/<template>")]
-// pub fn download(template: &str) -> (Status, (ContentType, Vec<u8>)) {
-//     let mut tar = tar::Builder::new(Vec::new());
-//     tar.append_dir_all(template, format!("./data/templates/{template}"))
-//         .unwrap();
+async fn download(Path(template): Path<String>) -> impl IntoResponse {
+    let mut tar = tar::Builder::new(Vec::new());
+    tar.append_dir_all(template.as_str(), format!("./data/templates/{template}"))
+        .unwrap();
 
-//     (Status::Ok, (ContentType::TAR, tar.into_inner().unwrap()))
-// }
+    let bytes = Bytes::from(tar.into_inner().unwrap());
+    let body = Full::new(bytes);
 
-async fn root() -> (StatusCode, Json<Value>) {
-    (StatusCode::OK, json!({ "status": "ok" }).into())
+    Response::builder()
+        .header(header::CONTENT_TYPE, "application/x-tar")
+        .header(
+            header::CONTENT_DISPOSITION,
+            format!("attachment; filename=\"{template}.tar\""),
+        )
+        .body(body)
+        .unwrap()
 }
 
 pub fn router() -> Router {
-    Router::new().route("/", get(root))
+    Router::new()
+        .route("/", get(list))
+        .route("/:template", get(download))
 }
