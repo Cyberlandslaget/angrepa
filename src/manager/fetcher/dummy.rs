2c2a3186a8f5ee85c02203a040a5ff3f402b9951
Andreas Røed Tvete <andreas.roed.tvete@gmail.com>
2023-07-26 04:16:54 +0200

    dummy fetcher returns a service for each nonrandom ip

--- src/manager/fetcher/dummy.rs
+++ src/manager/fetcher/dummy.rs
@@ -11,49 +11,28 @@ pub struct DummyFetcher {}
 #[async_trait]
 impl Fetcher for DummyFetcher {
     async fn services(&self) -> Result<HashMap<String, Service>, color_eyre::Report> {
-        // do it with an iterator and collect
-
-        let random_ip = || {
-            let mut rng = rand::thread_rng();
-            format!(
-                "{}.{}.{}.{}",
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-                rng.gen_range(0..255)
-            )
-        };
-
-        let test_tick = json! {["user49", "user20"]};
-
-        let mut ticks = HashMap::new();
-        ticks.insert(5, test_tick);
-
-        let ticks = Ticks(ticks);
-
-        let test_service = [(random_ip(), ticks)]
-            .into_iter()
-            .collect::<HashMap<_, _>>();
-        let test_service = Service(test_service);
-
         let mut map = HashMap::new();
-        map.insert("testservice".to_string(), test_service);
+        let mut test_service = HashMap::new();
+
+        self.ips().await?.into_iter().for_each(|ip| {
+            let mut rng = rand::thread_rng();
+            let test_tick = json! {[format!("user{}", rng.gen_range(0..=100)), format!("user{}", rng.gen_range(0..=100))]};
+
+            let mut ticks = HashMap::new();
+            ticks.insert(5, test_tick);
+
+            let ticks = Ticks(ticks);
+
+            test_service.insert(ip, ticks);
+
+        });
+
+        map.insert("testservice".to_string(), Service(test_service));
 
         Ok(map)
     }
 
     async fn ips(&self) -> Result<Vec<String>, color_eyre::Report> {
-        let random_ip = || {
-            let mut rng = rand::thread_rng();
-            format!(
-                "{}.{}.{}.{}",
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-                rng.gen_range(0..255)
-            )
-        };
-
-        Ok((0..5).map(|_| random_ip()).collect())
+        Ok((1..=10).map(|i| format!("10.0.{i}.1")).collect())
     }
 }
