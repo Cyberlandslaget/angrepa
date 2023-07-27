30d63827d3594a75f8b47c0a7042421e99c78fda
Jonas <jonas@jole.xyz>
2023-07-27 15:29:34 +0200

    move exploit commands into subcommand and file

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,19 +1,15 @@
 use argh::{self, FromArgs};
 use color_eyre::{eyre::eyre, Report};
-use colored::Colorize;
-use reqwest::{
-    multipart::{Form, Part},
-    Url,
-};
-use serde_json::json;
+use reqwest::Url;
 use std::{
     io::{Cursor, Read},
-    path::{Path, PathBuf},
+    path::Path,
     process::exit,
 };
 use tar::Archive;
 use tokio::time::Instant;
 
+pub mod exploit;
 pub mod template;
 
 #[derive(FromArgs, Debug)]
@@ -37,10 +33,7 @@ pub struct Args {
 /// commands
 enum Command {
     Ping(Ping),
-    Upload(Upload),
-    Download(Download),
-    Start(Start),
-    Stop(Stop),
+    Exploit(exploit::Exploit),
     Template(template::Template),
 }
 
@@ -48,62 +41,6 @@ enum Command {
 #[argh(subcommand, name = "ping")]
 /// ping the server
 struct Ping {}
-
-#[derive(FromArgs, Debug)]
-#[argh(subcommand, name = "upload")]
-/// upload an exploit
-struct Upload {
-    #[argh(positional)]
-    /// path to the exploit
-    exploit: PathBuf,
-
-    #[argh(option)]
-    /// name of service
-    service: Option<String>,
-
-    #[argh(option)]
-    /// name of exploit
-    name: Option<String>,
-
-    #[argh(option)]
-    /// pool size
-    pool: Option<usize>,
-
-    #[argh(option)]
-    /// blacklist, ex. "10.0.0.1, 10.0.0.2"
-    blacklist: Option<String>,
-}
-
-#[derive(FromArgs, Debug)]
-#[argh(subcommand, name = "download")]
-/// download an exploit
-struct Download {
-    #[argh(positional)]
-    /// exploit id
-    id: i32,
-
-    #[argh(option)]
-    /// directory to place the exploit folder in
-    path: PathBuf,
-}
-
-#[derive(FromArgs, Debug)]
-#[argh(subcommand, name = "start")]
-/// start an exploit
-struct Start {
-    #[argh(positional)]
-    /// id of exploit
-    id: i32,
-}
-
-#[derive(FromArgs, Debug)]
-#[argh(subcommand, name = "stop")]
-/// stop an exploit
-struct Stop {
-    #[argh(positional)]
-    /// id of exploit
-    id: i32,
-}
 
 #[tokio::main]
 async fn main() {
@@ -119,10 +56,7 @@ async fn main() {
 
     match &args.cmd {
         Command::Ping(ping) => ping.run(&args).await,
-        Command::Upload(upload) => upload.run(&args).await,
-        Command::Download(download) => download.run(&args).await,
-        Command::Start(start) => start.run(&args).await,
-        Command::Stop(stop) => stop.run(&args).await,
+        Command::Exploit(expl) => expl.run(&args).await,
         Command::Template(templ) => templ.run(&args).await,
     }
 }
@@ -163,90 +97,6 @@ fn tarify(path: &str) -> Result<Vec<u8>, Report> {
     tar.finish()?;
 
     Ok(tar.into_inner()?)
-}
-
-impl Upload {
-    async fn run(&self, args: &Args) {
-        // make sure all arguments are valid
-        std::fs::read_dir(&self.exploit).expect("failed to read exploit directory");
-        let name = self.name.as_ref().expect("missing name");
-        let service = self.service.as_ref().expect("missing service");
-        let blacklist: Vec<_> = self
-            .blacklist
-            .as_ref()
-            .map(|text| text.split(',').map(|ip| ip.trim()).collect())
-            .unwrap_or(vec![]);
-        let pool = self.pool;
-
-        // build tar
-        let tar = tarify(self.exploit.to_str().unwrap()).expect("failed to tar exploit");
-
-        println!("Uploading {}B file", tar.len());
-
-        // upload
-        let client = reqwest::Client::new();
-        let url = args.host.join("/exploit/upload").unwrap();
-
-        let config = json!({
-            "service": service,
-            "name": name,
-            "blacklist": blacklist,
-            "pool": pool,
-        });
-
-        let form = Form::new()
-            .text("config", config.to_string())
-            .part("tar", Part::bytes(tar).file_name("exploit.tar"));
-
-        let resp = client
-            .post(url.clone())
-            .multipart(form)
-            .send()
-            .await
-            .unwrap();
-
-        #[derive(serde::Deserialize)]
-        struct BuildResponse {
-            id: i32,
-            build_log: String,
-        }
-
-        let response = resp.text().await.unwrap();
-
-        let generic: GenericResponse = serde_json::from_str(&response).unwrap();
-
-        if generic.status == "ok" {
-            let build: BuildResponse = serde_json::from_str(&response).unwrap();
-            println!("{}", build.build_log.trim().blue());
-            println!("Successfully built exploit {}", build.id);
-        } else {
-            println!("Failed to build: '{}'", generic.message.unwrap_or_default());
-        }
-    }
-}
-
-impl Download {
-    async fn run(&self, args: &Args) {
-        // make sure all arguments are valid
-        std::fs::read_dir(&self.path).expect("failed to read output directory. does it exist?");
-        let dirname = format!("download_{}", self.id);
-        let out_dir = self.path.join(dirname);
-        if out_dir.exists() {
-            println!("Warning: {} already exists!", out_dir.display());
-        }
-        std::fs::create_dir_all(&out_dir).unwrap();
-
-        // download
-        let client = reqwest::Client::new();
-        let endpoint = format!("/exploit/download/{}", self.id);
-        let url = args.host.join(&endpoint).unwrap();
-
-        let tar = client.get(url).send().await.unwrap().bytes().await.unwrap();
-
-        // untar
-        let untarrer = Untarrer { data: tar.into() };
-        untarrer.untar(&out_dir).unwrap();
-    }
 }
 
 pub struct Untarrer {
@@ -290,61 +140,5 @@ struct GenericResponse {
 impl GenericResponse {
     fn is_ok(&self) -> bool {
         self.status == "ok"
-    }
-}
-
-impl Start {
-    async fn run(&self, args: &Args) {
-        let client = reqwest::Client::new();
-
-        let endpoint = format!("/exploit/start/{}", self.id);
-        let url = args.host.join(&endpoint).unwrap();
-
-        let resp: GenericResponse = client
-            .post(url.clone())
-            .send()
-            .await
-            .unwrap()
-            .json()
-            .await
-            .unwrap();
-
-        if resp.is_ok() {
-            println!("Started exploit {}", self.id);
-        } else {
-            println!(
-                "Failed to start exploit {}: {}",
-                self.id,
-                resp.message.unwrap_or_default()
-            );
-        }
-    }
-}
-
-impl Stop {
-    async fn run(&self, args: &Args) {
-        let client = reqwest::Client::new();
-
-        let endpoint = format!("/exploit/stop/{}", self.id);
-        let url = args.host.join(&endpoint).unwrap();
-
-        let resp: GenericResponse = client
-            .post(url.clone())
-            .send()
-            .await
-            .unwrap()
-            .json()
-            .await
-            .unwrap();
-
-        if resp.is_ok() {
-            println!("Stopped exploit {}", self.id);
-        } else {
-            println!(
-                "Failed to stop exploit {}: {}",
-                self.id,
-                resp.message.unwrap_or_default()
-            );
-        }
     }
 }
