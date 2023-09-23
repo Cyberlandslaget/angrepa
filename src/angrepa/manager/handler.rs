dc2c0081a3405a5f41083a8590ff3e82b6547a70
Jonas <jonas@jole.xyz>
2023-09-23 19:32:36 +0200

    fix flags

--- src/angrepa/manager/handler.rs
+++ src/angrepa/manager/handler.rs
@@ -5,19 +5,15 @@ use tracing::info;
 use super::submitter::{FlagStatus, Submitter};
 
 /// Submits flags
-async fn submit(submitter: impl Submitter + Send + Sync + Clone + 'static, db_url: String) {
+async fn submit(
+    submitter: impl Submitter + Send + Sync + Clone + 'static,
+    db_url: String,
+    flag_strings: Vec<String>,
+) {
     let mut conn = db_connect(&db_url).unwrap();
     let mut db = Db::new(&mut conn);
 
-    // extract out flags from the queue, then delete them
-    let flags = db.get_unsubmitted_flags().unwrap();
-
-    let flag_strings = flags.iter().map(|f| f.text.clone()).collect::<Vec<_>>();
-
     let results = submitter.submit(flag_strings).await.unwrap();
-    for flag in flags {
-        db.set_flag_submitted(flag.id).unwrap();
-    }
 
     let accepted = results
         .iter()
@@ -38,12 +34,25 @@ async fn submit(submitter: impl Submitter + Send + Sync + Clone + 'static, db_ur
 }
 
 pub async fn run(submitter: impl Submitter + Send + Sync + Clone + 'static, db_url: &str) {
+    let mut conn = db_connect(&db_url).unwrap();
+    let mut db = Db::new(&mut conn);
+
     // submit every 1s
     let mut send_signal = tokio::time::interval(std::time::Duration::from_secs(1));
     send_signal.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
 
     loop {
         send_signal.tick().await;
-        spawn(submit(submitter.clone(), db_url.to_owned()));
+
+        // extract out flags from the queue, then delete them
+        let flags = db.get_unsubmitted_flags().unwrap();
+        let flag_strings = flags.iter().map(|f| f.text.clone()).collect::<Vec<_>>();
+
+        // preemtively mark them as submitted
+        for flag in flags {
+            db.set_flag_submitted(flag.id).unwrap();
+        }
+
+        spawn(submit(submitter.clone(), db_url.to_owned(), flag_strings));
     }
 }
